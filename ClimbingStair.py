54c07888d11144796ea9e0d37160edbf9198a7d8
allencharp <liuzhidong_1985@hotmail.com>
2014-11-20 19:41:12 +0800

    Climbing Stair

--- ClimbingStair.py
+++ ClimbingStair.py
@@ -15,7 +15,6 @@ class Solution:
             return n
         else:
             n1Key = n-1
-
             n1Value = 0;
             if(self.dict.has_key(n1Key)):
                 n1Value = self.dict[n1Key]
@@ -23,14 +22,13 @@ class Solution:
                 n1Value = self.climbStairs(n1Key)
                 self.dict[n1Key] = n1Value
         
-                n2Key = n-2
-
-                n2Value = 0;
-                if(self.dict.has_key(n2Key)):
-                    n2Value = self.dict[n2Key]
-                else:
-                    n2Value = self.climbStairs(n2Key)
-                    self.dict[n2Key] = n2Value
+            n2Key = n-2
+            n2Value = 0;
+            if(self.dict.has_key(n2Key)):
+                n2Value = self.dict[n2Key]
+            else:
+                n2Value = self.climbStairs(n2Key)
+                self.dict[n2Key] = n2Value
             return n1Value + n2Value
            
 s = Solution()
