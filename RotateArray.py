2ceecfc67fc96779e5b84f80c47f7c0f50a7228f
allen <liuzhidong_1985@hotmail.com>
2015-05-03 21:15:11 +0800

    mmmm

---
+++ RotateArray.py
@@ -0,0 +1,22 @@
+# Rotate an array of n elements to the right by k steps.
+
+# For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+# Note:
+# Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+# [show hint]
+
+# Related problem: Reverse Words in a String II
+
+# Credits:
+# Special thanks to @Freezen for adding this problem and creating all test cases.
+class Solution:
+	# @param nums, a list of integer
+	# @param k, num of steps
+	# @return nothing, please modify the nums list in-place.
+	def rotate(self, nums, k):
+		if(len(nums) <= k):
+			return
+		for i in range(len(nums)):
+			
