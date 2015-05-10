b19fb28f768033103afcfa299870f457cade5089
allen <liuzhidong_1985@hotmail.com>
2015-05-10 21:06:36 +0800

    to be continued....

--- CouseSchedule.py
+++ CouseSchedule.py
@@ -21,7 +21,6 @@ class TempCourse:
 		course = ""
 
 class Solution:	
-	tempList = []
 	# @param {integer} numCourses
 	# @param {integer[][]} prerequisites
 	# @return {boolean}
@@ -33,36 +32,76 @@ class Solution:
 				return False
 		return True			
 	# @param {storeList} 
-	def UpdateStoreList(self, storeList, courses):
-		
+	def UpdateStoreList(self, storelist, courses):
+		tempList = []
 		for i,course in enumerate(courses):			
 			temp = TempCourse()
 			temp.course = course
-			if(self.ExistInStoreList(storeList, course) is not -1):
-				newIndex = self.MoveInStoreList(storelist, self.tempList)
+			newIndex = 0
+			where = self.ExistInStoreList(storelist, course)
+			
+			if(where is -1):
+				newIndex = self.MoveInStoreList(storelist, tempList, course)
 			else:
-				newIndex = self.RefreshCousesList(storelist, self.tempList, course)
+				newIndex = self.RefreshCousesList(storelist, tempList, course,where, i)
+
 			temp.originLoc = i
 			temp.newLoc = newIndex
-			self.tempList.append(temp)
-			if not self.IsSuccess(self.tempList):
+			tempList.append(temp)
+			if not self.IsSuccess(tempList):
 				return False
 		return True;
 
 	
-	def ExistInStoreList(self, storeList, course):
-		for i, items in storelist:
-			if(course in storelist):
+	def ExistInStoreList(self, storelist, course):
+		for i, items in enumerate(storelist):
+			if(course in items):
 				return i
 		return -1
 
-	def MoveInStoreList(self, storeList):
-		if len(storelist) == 0:
+	def MoveInStoreList(self, storelist, tempList,course):
+		if len(storelist) == 0: #or len(tempList) == 0:
+			temp = []
+			temp.append(course)
+			storelist.append(temp)
 			return 0
+		elif len(tempList) == 0:
+			storelist[0].append(course)
+			return 0
+		elif (len(storelist) <= tempList[len(tempList)-1].newLoc + 1):
+			temp = []
+			temp.append(course)
+			storelist.append(temp)
+			return len(storelist)-1
 		else:
-			return storeList[len(storelist)-1].newLoc + 1
+			storelist[tempList[len(tempList)-1].newLoc + 1].append(course)
+			return tempList[len(tempList)-1].newLoc + 1
 
-	def RefreshCousesList(self, storelist, course):
-		pass
-	def IsSuccess(self, couseList):
-		pass
+	def RefreshCousesList(self, storelist,tempList, course, where, currentIndex):
+		if(len(tempList) == 0):
+			return where
+		elif where == tempList[currentIndex-1].newLoc:
+			storelist[where].remove(course)
+			if(len(storelist)-1 == where):
+				temp = []
+				temp.append(course)
+				storelist.append(temp)
+			else:
+				storelist[where+1].append(course)
+			return where + 1
+		else:
+			return where
+
+	def IsSuccess(self, tempList):
+		for i,temp in enumerate(tempList):
+			cur = temp.newLoc;
+			for j in range(i,len(tempList)):
+				nex = tempList[j].newLoc
+				if(cur > nex):
+					return False
+		return True
+
+
+s = Solution()
+temp = [[1,2,3],[1,4,5],[1,2,4],[1,]]
+print s.canFinish(5, temp)
