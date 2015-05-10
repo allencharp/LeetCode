ed01dff1d2e17512bb5e286a9118f2f27046330d
allen <liuzhidong_1985@hotmail.com>
2015-05-10 19:06:11 +0800

    to be continue

--- CouseSchedule.py
+++ CouseSchedule.py
@@ -14,30 +14,55 @@
 # There are a total of 2 courses to take. To take course 1 you should have finished course 0, 
 # and to take course 0 you should also have finished course 1. So it is impossible.
 
+class TempCourse:
+	def __init__(self):
+		originLoc = 0;
+		newLoc = 0;
+		course = ""
+
 class Solution:	
+	tempList = []
 	# @param {integer} numCourses
 	# @param {integer[][]} prerequisites
 	# @return {boolean}
 	def canFinish(self, numCourses, prerequisites):
-		storelist = []
-		for i, courses in enumerate(prerequisites):
-			storeIndex = self.UpdateStoreList(storelist, storelist, courses)
+		coursestorelist = []
+		for courses in (prerequisites):
+			issucceed = self.UpdateStoreList(coursestorelist, courses)
+			if not issucceed:
+				return False
+		return True			
 	# @param {storeList} 
 	def UpdateStoreList(self, storeList, courses):
-		for course in courses:
-			if(self.ExistInStoreList(storeList, course) is not -1):
-				newIndex = self.MoveInStoreList(storelist, courses)
-			else:
-				self.InsertCouses(storelist, courses)
-
-	def InsertCouses(self, storelist, courses):
-		pass
 		
+		for i,course in enumerate(courses):			
+			temp = TempCourse()
+			temp.course = course
+			if(self.ExistInStoreList(storeList, course) is not -1):
+				newIndex = self.MoveInStoreList(storelist, self.tempList)
+			else:
+				newIndex = self.RefreshCousesList(storelist, self.tempList, course)
+			temp.originLoc = i
+			temp.newLoc = newIndex
+			self.tempList.append(temp)
+			if not self.IsSuccess(self.tempList):
+				return False
+		return True;
+
+	
 	def ExistInStoreList(self, storeList, course):
 		for i, items in storelist:
 			if(course in storelist):
 				return i
 		return -1
 
-	def MoveInStoreList(self, storeList, courses):
+	def MoveInStoreList(self, storeList):
+		if len(storelist) == 0:
+			return 0
+		else:
+			return storeList[len(storelist)-1].newLoc + 1
+
+	def RefreshCousesList(self, storelist, course):
+		pass
+	def IsSuccess(self, couseList):
 		pass
