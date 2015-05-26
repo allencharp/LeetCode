3eb89e19116cacbfd1f4e8172e212e43d9838b4c
allen <liuzhidong_1985@hotmail.com>
2015-05-26 20:34:06 +0800

    hash dict

---
+++ ContainsDuplicate.py
@@ -0,0 +1,19 @@
+# Given an array of integers, find if the array contains any duplicates. 
+# Your function should return true if any value appears at least twice in the array, 
+# and it should return false if every element is distinct.
+
+class Solution:
+	# @param {integer[]} nums
+	# @return {boolean}
+	def containsDuplicate(self, nums):
+		dict = {}
+		for i,num in enumerate(nums):
+			if num in dict:
+				return False
+			else:
+				dict[num]=i
+		return True
+
+list = [1,2,2]
+s = Solution();
+print s.containsDuplicate(list)
