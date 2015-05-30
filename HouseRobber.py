be104cbe0b875cb557dbd3f2586841c1667bf0a8
allen <liuzhidong_1985@hotmail.com>
2015-05-30 22:23:21 +0800

    wrong idea

--- HouseRobber.py
+++ HouseRobber.py
@@ -12,4 +12,37 @@ class Solution:
 	# @param {integer[]} nums
 	# @return {integer}
 	def rob(self, nums):
-		pass;
+		if(len(nums) == 0):
+			return 0
+		elif(len(nums) == 1):
+			return nums[0]
+		elif(len(nums) == 2):
+			return max(nums[0],nums[1])
+		elif(len(nums) == 3):
+			return max(nums[1], nums[0]+nums[2])
+		
+		maxLoc, maxValue = self.getMaxAndLoc(nums)
+		if(maxLoc == 0):
+			next_nums = nums[maxLoc + 2:]
+			_,nextMax = self.getMaxAndLoc(next_nums)
+			return maxValue + nextMax
+		elif(maxLoc == len(nums)-1):
+			next_nums = nums[:len(nums) - 2:]
+			_,nextMax = self.getMaxAndLoc(next_nums)
+			return maxValue + nextMax
+		else:
+			next_nums = nums[0:maxLoc-1] + nums[maxLoc+2: len(nums)]
+			_,nextMax = self.getMaxAndLoc(next_nums)
+			return max(maxValue + nextMax, nums[maxLoc-1]+nums[maxLoc+1])
+		
+	def getMaxAndLoc(self, num):
+		maxValue = 0
+		maxLoc = 0
+		for i, val in enumerate(num):
+			if(val > maxValue):
+				maxValue = val
+				maxLoc = i
+		return (maxLoc, maxValue)
+
+s = Solution()
+print s.rob([2,7,9,3,1])
