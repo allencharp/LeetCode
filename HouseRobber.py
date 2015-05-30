a1105bd23d9b34306a3b180310138a395310c898
allen <liuzhidong_1985@hotmail.com>
2015-05-30 22:30:56 +0800

    wrong 2

--- HouseRobber.py
+++ HouseRobber.py
@@ -12,37 +12,11 @@ class Solution:
 	# @param {integer[]} nums
 	# @return {integer}
 	def rob(self, nums):
-		if(len(nums) == 0):
-			return 0
-		elif(len(nums) == 1):
-			return nums[0]
-		elif(len(nums) == 2):
-			return max(nums[0],nums[1])
-		elif(len(nums) == 3):
-			return max(nums[1], nums[0]+nums[2])
+		nums_1 = [v for i,v in enumerate(nums) if i%2 == 1]
+		nums_2 = [v for i,v in enumerate(nums) if i%2 == 0]
 		
-		maxLoc, maxValue = self.getMaxAndLoc(nums)
-		if(maxLoc == 0):
-			next_nums = nums[maxLoc + 2:]
-			_,nextMax = self.getMaxAndLoc(next_nums)
-			return maxValue + nextMax
-		elif(maxLoc == len(nums)-1):
-			next_nums = nums[:len(nums) - 2:]
-			_,nextMax = self.getMaxAndLoc(next_nums)
-			return maxValue + nextMax
-		else:
-			next_nums = nums[0:maxLoc-1] + nums[maxLoc+2: len(nums)]
-			_,nextMax = self.getMaxAndLoc(next_nums)
-			return max(maxValue + nextMax, nums[maxLoc-1]+nums[maxLoc+1])
+		return max(sum(nums_1), sum(nums_2))
 		
-	def getMaxAndLoc(self, num):
-		maxValue = 0
-		maxLoc = 0
-		for i, val in enumerate(num):
-			if(val > maxValue):
-				maxValue = val
-				maxLoc = i
-		return (maxLoc, maxValue)
 
 s = Solution()
 print s.rob([2,7,9,3,1])
