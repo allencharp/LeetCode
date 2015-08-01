8be8b40d26dd6217606792b094597f21efe209dc
allen <liuzhidong_1985@hotmail.com>
2015-08-01 17:20:37 +0800

    low performance

--- ContainsDuplicateII.py
+++ ContainsDuplicateII.py
@@ -7,4 +7,23 @@ class Solution:
 	# @param {integer} k
 	# @return {boolean}
 	def containsNearbyDuplicate(self, nums, k):
+		for loc, value in enumerate(nums):
+			if(loc is not len(nums)-1):
+				isExist = self.containsDuplicateInlenght(value, nums[loc+1:], min(k, len(nums[loc+1:])))
+				if(isExist):
+					return True
+		return False
+			
+			
+	def containsDuplicateInlenght(self,value, num, len):
 		
+		isExist = False;
+		for i in range(len):
+			if num[i] == value:
+				isExist = True
+				break
+		return isExist
+	
+s = Solution()
+
+print s.containsNearbyDuplicate([1,2,3,4,5,1], 4)
