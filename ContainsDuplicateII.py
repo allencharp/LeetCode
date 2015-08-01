050d2d2315ce6ca8486bfb0f1f6a029ffaa6eae8
allen <liuzhidong_1985@hotmail.com>
2015-08-01 16:40:53 +0800

    commit

---
+++ ContainsDuplicateII.py
@@ -0,0 +1,10 @@
+#Given an array of integers and an integer k, 
+#find out whether there there are two distinct 
+#indices i and j in the array such that 
+#nums[i] = nums[j] and the difference between i and j is at most k.
+class Solution:
+	# @param {integer[]} nums
+	# @param {integer} k
+	# @return {boolean}
+	def containsNearbyDuplicate(self, nums, k):
+		
