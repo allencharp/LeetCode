26d97d28dad33a09f26c61c5f0f29e7d9b887c39
allen <liuzhidong_1985@hotmail.com>
2015-09-30 23:05:08 +0800

    catalan number

--- DifferentWaystoAddParentheses.py
+++ DifferentWaystoAddParentheses.py
@@ -21,22 +21,60 @@
 # Output: [-34, -14, -10, -10, 10]
 
 class Solution(object):
-    #def Solution(self):
-    operator = ["+", "-", "*"]
-    numList = []
-    operList = []
+
+    def __init__(self):
+        self.operator = ["+", "-", "*"]
+        self.numList = []
+        self.operList = []
+        self.rtnDict = {} # {key, [value1, value2]}
 
     def diffWaysToCompute(self, input):
 
         self.getNumber(input)
         self.getOperator(input)
 
+        n = len(self.numList)
+
+        if n == 0:
+            self.rtnDict[0] = []
+            self.rtnDict[0].append(0)
+            return self.rtnDict[0]
+        elif(n == 1):
+            self.rtnDict[1] = []
+            self.rtnDict[1].append(self.numList[0])
+            return self.rtnDict[1]
+        elif(n == 2):
+            num1 = self.numList[0]
+            num2 = self.numList[1]
+            operator = self.operList[0]
+            self.rtnList[2] = []
+            self.rtnList[2].append(self.getResult(num1, num2, operator))
+            return self.rtnDict[2]
+        else:
+            numadd = self.numList[len(self.numList) - 1]
+            numlast = self.numList[len(self.numList) - 2]
+
+
+
+            pass
+
 
         """
         :type input: str
         :rtype: List[int]
         """
-        pass
+        return rtn
+    def getResult(self, num1str, num2str, operator):
+        num1 = (int)(num1str)
+        num2 = (int)(num2str)
+
+        if(operator == "+"):
+            return num1 + num2
+        elif(operator == "*"):
+            return num1 * num2
+        else:
+            return num1-num2
+
     def getNumber(self, input):
         import re
         self.numList = re.split("\-|\+|\*", input)
@@ -46,14 +84,14 @@ class Solution(object):
         for i in range(len(input)):
             if input[i] == "+" or input[i] == "-" or input[i] == "*":
                 self.operList.append(input[i])
-
-
-    def catalan_number(n):
-        nm = dm = 1
-        for k in range(2, n+1):
-          nm, dm = ( nm*(n+k), dm*k )
-        return nm/dm
+    #
+    # def catalan_number(n):
+    #     nm = dm = 1
+    #     for k in range(2, n+1):
+    #       nm, dm = ( nm*(n+k), dm*k )
+    #     return nm/dm
 
 s = Solution()
-s.diffWaysToCompute("2*3-4*5")
-print s.operList
+print s.diffWaysToCompute("2")
+print s.rtnDict
+
