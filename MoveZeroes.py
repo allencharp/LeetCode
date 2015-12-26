14d17caf62f544bfb9cc16131dd778e967978c9d
allen <liuzhidong_1985@hotmail.com>
2015-12-26 16:36:04 +0800

    error with scenario [0,0,1]

--- MoveZeroes.py
+++ MoveZeroes.py
@@ -7,4 +7,22 @@
 # Minimize the total number of operations.
 class Solution(object):
     def moveZeroes(self, nums):
-        pass
+
+        if len(nums) == 0 or len(nums) == 1:
+            return nums
+
+        for loc in range(len(nums)):
+            if(nums[loc] == 0 and loc != len(nums) - 1):
+                self.moveZeroToLast(loc, nums)
+        return nums
+
+
+    def moveZeroToLast(self, loc, nums):
+        for i in range(loc, len(nums)-1):
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+
+
+s = Solution()
+print s.moveZeroes([0,0,1])
+
+
