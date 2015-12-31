892d6cc3fbbe6ea741fc76e51425faa9c00f120e
allen <liuzhidong_1985@hotmail.com>
2015-12-31 22:44:03 +0800

    lexicographical order

--- RemoveDuplicateLetters.py
+++ RemoveDuplicateLetters.py
@@ -8,36 +8,31 @@
 #
 # Given "cbacdcbc"
 # Return "acdb"
-import collections
 
 class Solution(object):
 	def removeDuplicateLetters(self, s):
-		ans = ''
-		for x in range(len(set(s))):
-			top, idx = s[0], 0
-			counter = collections.Counter(s)
-			for y in range(len(s)):
-				if top > s[y]:
-					top, idx = s[y], y
-				if counter[s[y]] == 1:
-					break
-				counter[s[y]] -= 1
-			ans += top
-			s = s[idx+1:].replace(top,'')
-		return ans
-		# rtnList = []
-		#
-		# for i in range(len(s)-1, 0, -1):
-		# 	alph = s[i]
-		# 	if alph not in rtnList:
-		# 		rtnList.append(alph)
-		#
-		# # what is lexicographical order ?
-		# # https://en.wikipedia.org/wiki/Lexicographical_order
-		# # e.g. a < aa < aaa < ab < abb < abc < b < bcd < be < ...
-		# rtnList.reverse()
-		#
-		# return "".join(rtnList)
+		rtnList = []
+
+		for i in range(len(s)):
+			alph = s[i]
+			if alph not in rtnList:
+				rtnList.append(alph)
+			else:
+				if(len(rtnList) == 0):
+					continue;
+
+				for loc, val in enumerate(rtnList):
+					if val == alph and loc != len(rtnList) - 1:
+
+						if(rtnList[loc+1] < val):
+							rtnList.remove(val)
+							rtnList.append(val)
+
+		# what is lexicographical order ?
+		# https://en.wikipedia.org/wiki/Lexicographical_order
+		# e.g. a < aa < aaa < ab < abb < abc < b < bcd < be < ...
+
+		return "".join(rtnList)
 
 s = Solution()
-print s.removeDuplicateLetters("cbacdcbc")
+print s.removeDuplicateLetters("bcabc")
