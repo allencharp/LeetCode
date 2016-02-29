b2b80e05954e3eaedfcf6ac56aec170c7e3ac9ba
allen <liuzhidong_1985@hotmail.com>
2016-02-29 22:36:25 +0800

    ugly code, time exceed

--- SelfCrossing.py
+++ SelfCrossing.py
@@ -4,31 +4,61 @@
 #
 # Example 1:
 # Given x = [2, 1, 1, 2],
-# ┌───┐
-# │   │
-# └───┼──>
-#     │
+
 #
 # Return true (self crossing)
 # Example 2:
 # Given x = [1, 2, 3, 4],
-# ┌──────┐
-# │      │
-# │
-# │
-# └────────────>
+
 #
 # Return false (not self crossing)
 # Example 3:
 # Given x = [1, 1, 1, 1],
-# ┌───┐
-# │   │
-# └───┼>
+
 #
 # Return true (self crossing)
 class Solution(object):
+	def __init__(self):
+		self.route = [(0,0)]
+		self.currentX = 0
+		self.currentY = 0
+
 	def isSelfCrossing(self, x):
-		"""
-		:type x: List[int]
-		:rtype: bool
-		"""
+
+		if(len(x) < 4):
+			return False
+
+		for key in range(len(x)):
+			val = x[key]
+			if key % 4 == 0:
+				for i in range((val)):
+					self.currentY = self.currentY + 1
+					if (self.currentX, self.currentY) in self.route:
+						return True;
+					else:
+						self.route.append((self.currentX, self.currentY))
+			elif key % 4 == 1:
+				for i in range((val)):
+					self.currentX = self.currentX - 1
+					if (self.currentX, self.currentY) in self.route:
+						return True;
+					else:
+						self.route.append((self.currentX, self.currentY))
+			elif key % 4 == 2:
+				for i in range((val)):
+					self.currentY = self.currentY - 1
+					if (self.currentX, self.currentY) in self.route:
+						return True;
+					else:
+						self.route.append((self.currentX, self.currentY))
+			else:
+				for i in range((val)):
+					self.currentX = self.currentX + 1
+					if (self.currentX, self.currentY) in self.route:
+						return True;
+					else:
+						self.route.append((self.currentX, self.currentY))
+		return False
+
+s = Solution()
+print s.isSelfCrossing([2,1,1,2])
