5039c44ce8847ec0d99aa8ec74f8f2ec7d009e6e
allen <liuzhidong_1985@hotmail.com>
2016-07-22 11:35:06 +0800

    unique digit

--- CountNumbersWithUniqueDigits.py
+++ CountNumbersWithUniqueDigits.py
@@ -1,4 +1,4 @@
-# Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+# Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10 nn.
 #
 # Example:
 # Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ≤ x < 100
@@ -7,6 +7,14 @@
 class Solution(object):
 	def countNumbersWithUniqueDigits(self, n):
 		"""
-        :type n: int
-        :rtype: int
-        """
+		:type n: int
+		:rtype: int
+		"""
+		if len(str(n)) == 0:
+			return 0
+		elif len(str(n)) == 1:
+			return 0
+		elif len(str(n)) == 2:
+			return 9
+		else:
+			pass
