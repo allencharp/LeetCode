729811c74d9c1f8ae9a8ddc848fba16ad64ebed8
allen <liuzhidong_1985@hotmail.com>
2016-09-04 21:35:54 +0800

    init

---
+++ FirstUniqueCharacterinaString.py
@@ -0,0 +1,24 @@
+# Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+#
+# Examples:
+#
+# s = "leetcode"
+# return 0.
+#
+# s = "loveleetcode",
+# return 2.
+# Note: You may assume the string contain only lowercase letters.
+class Solution(object):
+	
+	def __init__(self):
+		self.repeatList = []
+	
+	def firstUniqChar(self, s):
+		"""
+		:type s: str
+		:rtype: int
+		"""
+		pass
+	
+	def cache(self, *args):
+		pass
