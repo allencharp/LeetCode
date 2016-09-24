db9bee6ebc1b96215c1d0c233f33575f67acdd28
allen <liuzhidong_1985@hotmail.com>
2016-09-24 22:36:38 +0800

    bug exists

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -12,18 +12,29 @@
 
 class Solution(object):
 	
-
-	
 	def lengthOfLongestSubstring(self, s):
 		
 		if len(s) <= 1:
 			return len(s)
 		
+		max_len = 1
+		longest_str_set = set()
 		for loc, val in enumerate(s):
 			longest_str_set = longest_str_set | set(val)
 			
-			for i,v in enumerate(s[loc:]):
+			for i,v in enumerate(s[loc+1:]):
 				if v in longest_str_set:
+					
+					if len(longest_str_set) > max_len:
+						max_len = len(longest_str_set)
+					
+					longest_str_set = set()
 					break;
 				else:
 					longest_str_set |= set(v)
+					
+		return max_len
+		
+a = "au"
+s = Solution()
+print s.lengthOfLongestSubstring(a)
