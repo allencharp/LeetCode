1d456dadd7c46a3415de915a562863ba9cbb2f1f
allen <liuzhidong_1985@hotmail.com>
2017-03-08 22:35:26 +0800

    solution got

---
+++ BasicCalculator.py
@@ -0,0 +1,22 @@
+# Implement a basic calculator to evaluate a simple expression string.
+#
+# The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+#
+# You may assume that the given expression is always valid.
+#
+# Some examples:
+# "1 + 1" = 2
+# " 2-1 + 2 " = 3
+# "(1+(4+5+2)-3)+(6+8)" = 23
+
+class Solution(object):
+    
+    def __init__(self):
+        self.oper_with_backet_stack = []
+        self.oper_without_backet_stack = []
+    
+    def calculate(self, s):
+        pass
+    
+    def calculate_without_backet(self, s):
+        pass
