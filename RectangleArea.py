4e6135a8a2f9e2550c3470d02c818eebe510862c
allen <liuzhidong_1985@hotmail.com>
2017-05-14 17:20:47 +0800

    rectangle area

---
+++ RectangleArea.py
@@ -0,0 +1,22 @@
+# Find the total area covered by two rectilinear rectangles in a 2D plane.
+#
+# Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+#
+# Rectangle Area
+# Assume that the total area is never beyond the maximum possible value of int.
+#
+# https://leetcode.com/problems/rectangle-area/#/description
+
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
