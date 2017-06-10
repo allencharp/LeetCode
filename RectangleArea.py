d5d6f12e73fdf7fcaf7036ea6b5a415219914fc1
allen <liuzhidong_1985@hotmail.com>
2017-06-10 17:50:13 +0800

    computeArea

--- RectangleArea.py
+++ RectangleArea.py
@@ -10,13 +10,27 @@
 class Solution(object):
     def computeArea(self, A, B, C, D, E, F, G, H):
         """
-        :type A: int
-        :type B: int
-        :type C: int
-        :type D: int
-        :type E: int
-        :type F: int
-        :type G: int
-        :type H: int
+        :type A: int first left
+        :type B: int first bottom
+        :type C: int first right
+        :type D: int first top
+        :type E: int second left
+        :type F: int second bottom
+        :type G: int second right
+        :type H: int second top
         :rtype: int
         """
+        total = (C-A)*(D-B) + (G-E)*(H-F)
+        if (C <= E) or (A >= G) \
+                or B >= H or D <= F:
+            return total
+
+        left = max(A, E)
+        right = min(C,G)
+        top = min(D,H)
+        bottom = max(B,F)
+
+        return total - (right - left)*(top - bottom)
+
+s = Solution()
+print(s.computeArea(-3,0,3,4,0,-1,9,2))
