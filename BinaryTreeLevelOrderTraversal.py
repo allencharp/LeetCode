2a9c14547f9f29b5809c2edf0d83bc3c3d330e25
allen <liuzhidong_1985@hotmail.com>
2018-01-27 18:41:30 +0800

    solution

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -14,18 +14,25 @@
 #   [9,20],
 #   [15,7]
 # ]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# maintain two lists:
+# one is for the return value
+# one is maintain the binary tree level
 class Solution(object):
     def levelOrder(self, root):
+        rtn, level = [],[root]
+        while level:
+            rtn.append([node.val for node in level])
 
-        pos = 0
+            for node in level:
+                level.extend([node.left, node.right])
 
-        level = 0
-
-        rtnList = []
-
-        while root[pos] is not None:
-            times = 2**level
-            for i in range(times):
-
-
-        return rtnList
+s = Solution()
+print(s.levelOrder([3,9,20,"null","null",15,7]))
