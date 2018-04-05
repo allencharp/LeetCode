82202596753cde37938864cf102d9621cd4fe46a
allen <liuzhidong_1985@hotmail.com>
2018-04-05 22:25:51 +0800

    log2n

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -3,14 +3,25 @@
 # For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 # the contiguous subarray [4,-1,2,1] has the largest sum = 6
 
+
+
 class Solution(object):
     def maxSubArray(self, nums):
 
-        sum_list_num = []
+        max = 0
 
-        start_index, continue_index, end_index = 0
+        if not nums:
+            return 0
 
-        for index in range(len(nums)):
-            pass
+        for index1 in range(len(nums)):
+            for index2 in range(index1, len(nums)):
+                if max < sum(nums[index1: index1+index2]):
+                    max = sum(nums[index1: index1+index2])
+                    print(max)
 
-        return max(sum_list_num)
+        return max
+
+
+
+s = Solution()
+print(s.maxSubArray([4,-1,2,1]))
