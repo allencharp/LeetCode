d614e31700869c419235b198c7d5b11dd0014f3f
allen <liuzhidong_1985@hotmail.com>
2019-09-10 21:50:14 +0800

    Roman to Integer, very very ugly

--- NHighestSalary.sql
+++ NHighestSalary.sql
@@ -1,7 +1,27 @@
+--Write a SQL query to get the nth highest salary from the Employee table.
+--
+--+----+--------+
+--| Id | Salary |
+--+----+--------+
+--| 1  | 100    |
+--| 2  | 200    |
+--| 3  | 300    |
+--+----+--------+
+--For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+--
+--+------------------------+
+--| getNthHighestSalary(2) |
+--+------------------------+
+--| 200                    |
+--+------------------------+
+
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-      
+      select salary from
+      ()
+      limit 1
   );
 END
